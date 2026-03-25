13fa48f166c86e6dcdebd764cde2ae2d68f97ea8
TagirovAlex <tagirov.alex@gmail.com>
2026-03-25 14:30:21 +0300

    main

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask_limiter import Limiter
+from flask_migrate import Migrate
 from flask_limiter.util import get_remote_address
 from apscheduler.schedulers.background import BackgroundScheduler
 
